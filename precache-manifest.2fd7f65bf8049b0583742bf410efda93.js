5b6919a52bc8fb812e2ba958f8f7f4a4ee75db87
Ravindra R. Jaju <jaju@msync.org>
2021-10-13 19:09:09 +0530

    Updated build with a different node version

---
+++ precache-manifest.2fd7f65bf8049b0583742bf410efda93.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f43c84c0caf2c30222cca6eb4f359d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65213b04eaa4e765f6dc",
+    "url": "/static/css/main.7fbc9f91.chunk.css"
+  },
+  {
+    "revision": "7aa0802a6860f4c56a86",
+    "url": "/static/js/2.b4e26868.chunk.js"
+  },
+  {
+    "revision": "e7583a0a23d4f5abcf2246ba0154c9d0",
+    "url": "/static/js/2.b4e26868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65213b04eaa4e765f6dc",
+    "url": "/static/js/main.0f0a483d.chunk.js"
+  },
+  {
+    "revision": "d1ee916c586cd2d75ff6",
+    "url": "/static/js/runtime-main.d49955f1.js"
+  },
+  {
+    "revision": "b53eaec4c9655acf6be660817d9b3d7d",
+    "url": "/static/media/dropbox.b53eaec4.svg"
+  },
+  {
+    "revision": "f5fd3e02579be7701f2aeb2a5a3c68bc",
+    "url": "/static/media/google_drive.f5fd3e02.png"
+  },
+  {
+    "revision": "11553f69cf091fb729dad6100fde7393",
+    "url": "/static/media/organice.11553f69.svg"
+  }
+]);
